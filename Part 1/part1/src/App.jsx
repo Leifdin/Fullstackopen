eeb50b2317a37cb6cf477e79265aa5a2d34fc027
Pavol Polonec <pavolpolonec@gmail.com>
2024-06-11 15:29:23 +0200

    created react app for first part

---
+++ Part 1/part1/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://polonec.eu" target="_blank">
+          <img src="https://polonec.eu/p/wp-content/uploads/2020/03/cropped-logo-2-e1583225616808.png" className="logo" alt="Logo ZŠ Spojová" />
+        </a>
+      </div>
+      <h1>Vite + React + Polonec</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 100)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
