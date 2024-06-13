03b39859434a9d9c59147b9470f6d498ef6fbacc
Pavol Polonec <pavolpolonec@gmail.com>
2024-06-13 13:26:26 +0200

    exercise 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,34 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const Header = (props) => {
+  return(
+    <div>
+      <h1>{props.courseName}</h1>
+    </div>
+  )
+}
 
-function App() {
-  const [count, setCount] = useState(0)
+const Part = (props) => {
+  console.log("Rendering ", props.partName, props.exercisesCount)
+  return(
+    <p>{props.partName} has {props.exercisesCount} exercises</p>
+  )
+}
+
+const Content = (props) => {
+  return(
+    <div>
+      <Part partName={props.partName1} exercisesCount={props.exercisesCount1} />
+      <Part partName={props.partName2} exercisesCount={props.exercisesCount2} />
+      <Part partName={props.partName3} exercisesCount={props.exercisesCount3} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return(
+    <div>
+      <p>Number of exercises: {props.exercisesTotal}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Header courseName={course} />
+      <Content partName1={part1} exercisesCount1={exercises1} partName2={part2} exercisesCount2={exercises2} partName3={part3} exercisesCount3={exercises3} />
+      <Total exercisesTotal={exercises1 + exercises2 + exercises3} />
+    </div>
   )
 }
 
