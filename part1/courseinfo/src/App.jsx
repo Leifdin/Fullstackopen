da8221e639426bcf75a79a4df2480f651d6eddd1
Pavol Polonec <pavolpolonec@gmail.com>
2024-06-14 11:39:47 +0200

    finished exercise 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -16,35 +16,45 @@ const Part = (props) => {
 const Content = (props) => {
   return(
     <div>
-      <Part partName={props.partName1} exercisesCount={props.exercisesCount1} />
-      <Part partName={props.partName2} exercisesCount={props.exercisesCount2} />
-      <Part partName={props.partName3} exercisesCount={props.exercisesCount3} />
+    {props.course.parts.map((coursePart) => (
+      <Part key={coursePart.name} partName={coursePart.name} exercisesCount={coursePart.exercises} />
+    ))}
     </div>
+    /*I didn't have to use iteration for this component, however, it felt right to do so. So I did. I also wanted to try map() function*/
   )
 }
 
 const Total = (props) => {
   return(
     <div>
-      <p>Number of exercises: {props.exercisesTotal}</p>
+      <p>Number of exercises: {props.course.parts[0].exercises + props.course.parts[0].exercises + props.course.parts[0].exercises}</p>
     </div>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return (
     <div>
-      <Header courseName={course} />
-      <Content partName1={part1} exercisesCount1={exercises1} partName2={part2} exercisesCount2={exercises2} partName3={part3} exercisesCount3={exercises3} />
-      <Total exercisesTotal={exercises1 + exercises2 + exercises3} />
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   )
 }
