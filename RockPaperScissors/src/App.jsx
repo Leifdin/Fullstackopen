7b48e1bae1b43c00d05029df75cf80009fca334b
Pavol Polonec <pavolpolonec@gmail.com>
2024-06-15 15:38:15 +0200

    renamed playground as RockPaperScissors

--- RockPaperScissors/src/App.jsx
+++ RockPaperScissors/src/App.jsx
@@ -5,12 +5,23 @@ const Display = ({text, counter}) => <div className="display">{text}{counter}</d
 
 const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
+const History = (props) => {
+  if (props.allClicks.length === 0){
+    return (
+      <p>The app counts rock - paper - scissors score by clicking on the buttons</p>
+    )
+  }
+  return (
+    <p>Game history: {props.allClicks.join(' ')}</p>
+  )
+}
 
 const App = () => {
 
   const [pali, setPali] = useState(0)
   const [nika, setNika] = useState(0)
   const [allClicks, setAllClicks] = useState([])
+  const [ties, setTies] = useState(0)
 
   const handlePali = () => {
     console.log('Pali state before increasing: ', pali)
@@ -22,20 +33,32 @@ const App = () => {
     setAllClicks(allClicks.concat('N'))
     setNika (nika + 1)
   }
+  const handleTies = () => {
+    console.log('Ties state before decreasing: ', ties)
+    setAllClicks(allClicks.concat('T'))
+    setTies (ties + 1)
+  }
+
   const setToZero = () => {
     console.log('Counter state before zeroing: ', pali, nika)
     setPali(0)
     setNika(0)
+    setTies(0)
     setAllClicks([])
   }
 
   return (
     <div>
       <Display text='Pali: ' counter={pali} />
+      <Display text='Ties: ' counter={ties} />
       <Display text='Nika: ' counter={nika} />
-      <p>All clicks: {allClicks.join(' ')}</p>
+      <Display text='Total: ' counter={nika + pali + ties} />
+      <History allClicks={allClicks} />
       <Button onClick={handlePali}
       text='Pali'
+      />
+      <Button onClick={handleTies}
+      text='Tie'
       />
       <Button onClick={handleNika}
       text='Nika'
