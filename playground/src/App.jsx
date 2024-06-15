b6b275aecb1959730dabc7da5f3e8ee803493db6
Pavol Polonec <pavolpolonec@gmail.com>
2024-06-15 13:13:22 +0200

    Trying hooks and states

--- playground/src/App.jsx
+++ playground/src/App.jsx
@@ -1,29 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-const Hello = ({name, age}) => {
+const Display = ({counter}) => <div className="display">{counter}</div>
 
-  const bornYear = () => new Date().getFullYear() - age
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+
+
+const App = () => {
+
+  const [ counter, setCounter ] = useState(0)
+  console.log('Rendering with counter value ', counter)
+
+  const increaseByOne = () => {
+    console.log('Counter state before increasing: ', counter)
+    setCounter(counter + 1)
+  }
+  const decreaseByOne = () => {
+    console.log('Counter state before decreasing: ', counter)
+    setCounter (counter - 1)
+  }
+  const setToZero = () => {
+    console.log('Counter state before zeroing: ', counter)
+    setCounter(0)
+  }
 
   return (
     <div>
-      <p> Hello {name}, you are {age}</p>
-      <p> so you were probably bown in {bornYear()}</p>
-    </div>
-  )
-}
-
-const App = () => {
-  const name = 'Pavol'
-  const age = 30
-  
-  return(
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Veronika" age={19 + 10} />
-      <Hello name={name} age={age} />
+      <Display counter={counter} />
+      <Button onClick={increaseByOne}
+      text='plus'
+      />
+      <Button onClick={decreaseByOne}
+      text='minus'
+      />
+      <Button onClick={setToZero}
+      text='zero'
+      />
     </div>
   )
 }
