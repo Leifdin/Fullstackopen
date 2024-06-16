6b348ba2de80487a35b272cba9b1613cb1f82c1c
Pavol Polonec <pavolpolonec@gmail.com>
2024-06-16 13:42:37 +0200

    Finished handlers exercises

--- ReactEventHandlers/src/App.jsx
+++ ReactEventHandlers/src/App.jsx
@@ -3,6 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const Button = ({ text, handleClick }) => <button onClick={handleClick}>{text}</button>
+
+const Display = (props) => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
 function App() {
   const [value, setValue] = useState(10)
 
@@ -15,21 +25,15 @@ function App() {
     setValue(newValue)
   }
 
+  
+
   return (
     <div>
       {value}
-      <button onClick={() => setToValue('World')}>
-        World
-      </button>
-      <button onClick={() => setToValue(1000)}>
-        1000
-      </button>
-      <button onClick={() => setToValue(0)}>
-        Reset
-      </button>
-      <button onClick={() => setToValue(value + 1)}>
-        Plus
-      </button>
+      <Button text='World' handleClick={() => setToValue('World')} />
+      <Button text='1000' handleClick={() => setToValue(1000)} />
+      <Button text='Reset' handleClick={() => setToValue(0)} />
+      <Button text='Plus' handleClick={() => setToValue(value + 1)} />
     </div>
   )
 }
