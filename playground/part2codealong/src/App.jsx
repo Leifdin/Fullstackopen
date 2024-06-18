8df0df18aed375feeea7eba2c9c1a8796654e434
Pavol Polonec <pavolpolonec@gmail.com>
2024-06-18 14:35:42 +0200

    part 2

--- playground/part2codealong/src/App.jsx
+++ playground/part2codealong/src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const Note = ({note}) => <li>{note}</li>
+
 const App = ({notes}) => {
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note => <li key={note.id}>{note.content}</li>)}
+        {notes.map(note => <Note key={note.id} note={note.content} />)}
       </ul>
     </div>
   )
