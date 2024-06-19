2dd117f88e766391e1101fe57c720e691892f8dd
Pavol Polonec <pavolpolonec@gmail.com>
2024-06-19 13:25:27 +0200

    commit

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -26,10 +26,7 @@ const Content = (props) => {
 }
 
 const Total = ({course}) => {
-  let total = 0
-  for (let i = 0; i < course.parts.length; i++){
-    total += course.parts[i].exercises
-  }
+  const total = course.parts.reduce((accumulator, currentValue) => accumulator + currentValue)
   return(
     <div>
       <p>Number of exercises: {total}</p>
