fb351d58a14042e69edfb935b608ab88eb3cff27
Pavol Polonec <pavolpolonec@gmail.com>
2024-06-19 09:43:54 +0200

    edited playground, created courseinfo for exercises 2.1 - 2.5

--- playground/part2codealong/src/App.jsx
+++ playground/part2codealong/src/App.jsx
@@ -2,8 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Note from './components/Note.jsx'
 
-const Note = ({note}) => <li>{note}</li>
+
 
 const App = ({notes}) => {
   return (
