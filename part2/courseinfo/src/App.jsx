96ea05f6e3a3a1a7b2d8b694e11c120d062a7318
Pavol Polonec <pavolpolonec@gmail.com>
2024-06-20 09:43:31 +0200

    exercise 2.4

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,13 +1,13 @@
 const Header = (props) => {
   return(
     <div>
-      <h1>{props.courseName}</h1>
+      <h2>{props.course.name}</h2>
     </div>
   )
 }
 
 const Part = ({partName, exercisesCount}) => {
-  /*console.log('Rendering ', partName, exercisesCount)*/
+
 
   return(
     <p>{partName} has {exercisesCount} exercises</p>
@@ -17,17 +17,20 @@ const Part = ({partName, exercisesCount}) => {
 const Content = (props) => {
   return(
     <div>
-    {props.course.parts.map((coursePart) => (
-      <Part key={coursePart.id} partName={coursePart.name} exercisesCount={coursePart.exercises} />
-    ))}
+    {props.course.parts.map((coursePart) => {
+      console.log('Rendering ', coursePart.name, coursePart.exercises)
+      return (
+        <Part key={coursePart.id} partName={coursePart.name} exercisesCount={coursePart.exercises} />
+      )
+    }
+    )}
     </div>
-    /*I didn't have to use iteration for this component, however, it felt right to do so. So I did. I also wanted to try map() function*/
   )
 }
 
 const Total = ({course}) => {
   const total = course.parts.reduce((accumulator, currentValue) => {
-    /*console.log('Accumulator: ', accumulator, 'currentValue: ', currentValue.exercises)*/
+    console.log('Accumulator: ', accumulator, 'currentValue: ', currentValue.exercises)
     return (accumulator + currentValue.exercises)
   }, 0)
   return(
@@ -48,34 +51,55 @@ const Course = ({course}) => {
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'JavaScript Arrays',
-        exercises: 5,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
   return (
-    <Course course={course} />
+    <div>
+      <h1>Web development curriculum</h1>
+      {courses.map((course) => <Course course={course} />)}
+    </div>
   )
 }
 
