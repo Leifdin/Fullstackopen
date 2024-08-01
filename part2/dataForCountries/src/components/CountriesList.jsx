e8b3e4607a06ff5a90a64ff95285670f7d85ebe0
Pavol Polonec <pavolpolonec@gmail.com>
2024-08-01 16:40:24 +0200

    exercise 2.18 finished

--- part2/dataForCountries/src/components/CountriesList.jsx
+++ part2/dataForCountries/src/components/CountriesList.jsx
@@ -1,8 +1,25 @@
 import countries from "../services/countries"
 
-const Country = ( {countryName}) => {
+const Country = ( {displayCountry, format}) => {
+    if (format === "long") {
+        const countryLanguages = Object
+        .values(displayCountry.languages)
+        .map(language => <li key={language}>{language}</li>)
+        console.log(countryLanguages)
+        return (
+            <div>
+                <h1>{displayCountry.name.common}</h1>
+                <p>Capital: {displayCountry.capital[0]}</p>
+                <p>Population: {displayCountry.area}</p>
+                <h2>Languages:</h2>
+                <ul>
+                    {countryLanguages}
+                </ul>
+            </div>
+        )
+    }
     return (
-        <li>{countryName}</li>
+        <li>{displayCountry.name.common}</li>
     )
 }
 const CountriesList = ( {data, searchFilter}) => {
@@ -21,12 +38,18 @@ const CountriesList = ( {data, searchFilter}) => {
             </div>
         )
     }
+
+    if (displayCountries.length === 1) {
+        return (
+            <Country displayCountry={displayCountries[0]} format="long" />
+        )
+    }
     
     if (displayCountries.length <= 10) {
         return (
             <div>
                 <ul>
-                {displayCountries.map(country => <Country countryName={country.name.common} key={country.cca2} />)}
+                {displayCountries.map(country => <Country displayCountry={country} key={country.cca2} />)}
                 </ul>
                 
             </div>
