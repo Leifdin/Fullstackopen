54ec7336420338e080ff5f593cfeb8c9c852d92e
Pavol Polonec <pavolpolonec@gmail.com>
2024-08-13 10:31:56 +0200

    wip

--- part3/Exercises/index.js
+++ part3/Exercises/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require ('cors')
-
+const person = require('./models/person.js')
+//const note = require('../StudyMaterial/models/note')
 const app = express()
 
 morgan.token('id', (req, res) => {
@@ -16,31 +17,11 @@ app.use(morgan(':method :url :id :response-time :body'))
 app.use(cors())
 app.use(express.static('dist'))
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    person.find({}).then(persons => {
+      response.json(persons)
+    })
 })
 
 app.get('/info', (request, response) => {
---
+++ part3/Exercises/models/person.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const url = process.env.MONGODB_URI
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url)
+.then(result => {
+    console.log(`Connection successful`)
+})
+.catch (error => {
+    console.log(`Connection error: ${error.message}`)
+})
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = new mongoose.model('Person', personSchema)
+
+personSchema.set('toJson', {
+    transform: (document, returnedObject) => {
+        returnedObject.id=returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Person', personSchema)
