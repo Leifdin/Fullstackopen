485d7a0ac49ed8a1dc837e0847df639b4284b2c8
Pavol Polonec <pavolpolonec@gmail.com>
2024-08-13 10:11:18 +0200

    Study material

--- part3/StudyMaterial/index.js
+++ part3/StudyMaterial/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-
+const Note = require('./models/note')
+require('dotenv').config()
 app.use(cors())
 
 
@@ -21,45 +22,20 @@ const unknownEndpoint = (request, response) => {
 app.use(express.json())
 app.use(express.static('dist'))
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  },
-  {
-    id: "4",
-    content: "If this note shows up, deploy script to the server works",
-    important: true
-  }
-]
-
 app.get('/', (request, response) => {
     response.send('<h1>Hello, World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    const note = notes.find(note => note.id === id)
-    if (note) {
+    Note.findById(request.params.id).then(note => {
         response.json(note)
-    } else {
-        response.status(404).send('That note does not exist')
-    }
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -69,33 +45,22 @@ app.delete('/api/notes/:id', (request, response) => {
     
 })
 
-const generateId = () => {
-    const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
-    return String(maxId + 1)
-}
-
 app.post('/api/notes', (request, response) => {
-    
-    
+     
     const body = request.body
-
     if (!body.content) {
         return response.status(400).json({
             error: 'Content missing'
         })
     }
-    const note = {
+    const note = new Note ({
         content: body.content,
         important: Boolean(body.important) || false,
-        id: generateId()
-    }
+    })
     console.log(note)
-
-    notes = notes.concat(note)
-
-    response.json(note)
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 })
 
 app.use(unknownEndpoint)
