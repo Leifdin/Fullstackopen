1e81050f94c04a320a1653d4b17a3e424f83fce0
Leifdin <pavolpolonec@gmail.com>
2024-11-19 21:56:53 +0100

    study material

--- part4/StudyMaterial/controllers/notes.js
+++ part4/StudyMaterial/controllers/notes.js
@@ -1,8 +1,17 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 require('express-async-errors')
+
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')){
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 
 
@@ -30,7 +39,14 @@ notesRouter.delete('/:id', async (request, response) => {
 notesRouter.post('/', async (request, response) => {
 
   const body = request.body
-  const user = await User.findById(body.userId)
+
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if(!decodedToken.id){
+    return response.status(401).json({ error: 'invalid token' })
+  }
+
+
+  const user = await User.findById(decodedToken.id)
   if (!body.content) {
     return response.status(400).json({
       error: 'Content missing'
