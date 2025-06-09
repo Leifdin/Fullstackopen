5df14f1a560985bd0408db03a705f37e7909dfc1
Pavol Polonec <pavolpolonec@gmail.com>
2025-06-09 17:59:03 +0200

    study material redux basics

--- StudyMaterial/redux/src/App.jsx
+++ StudyMaterial/redux/src/App.jsx
@@ -1,13 +1,51 @@
-import { useState } from 'react'
-import './App.css'
+import { createStore } from 'redux'
 
-function App() {
-  const [count, setCount] = useState(0)
+const noteReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'NEW_NOTE': return [...state, action.payload]
+    case 'TOGGLE_IMPORTANCE': {
+      const id = action.payload.id
+      const noteToChange = state.find(note => note.id === id)
+      const changedNote = {
+        ...noteToChange,
+        important: !noteToChange.important
+      }
+      return state.map(note => note.id === id ? changedNote : note)
+    }
+    default: return state
+  }
+}
 
+const store = createStore(noteReducer)
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    content: 'the app state  is in redux store',
+    important: true,
+    id: 1,
+  }
+})
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    content: 'state changes are made with actions',
+    important: false,
+    id: 2,
+  }
+})
+const App = () => {
   return (
-    <>
-    </>
+    <div>
+      <ul>
+        {store.getState().map((note, i) =>
+          <li key={i}>
+            {note.content} <strong>{note.important ? 'important' : ''}</strong>
+          </li>
+        )}
+      </ul>
+    </div>
   )
 }
 
 export default App
+export { noteReducer }
