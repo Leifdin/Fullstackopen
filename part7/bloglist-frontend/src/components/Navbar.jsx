ca3f0b911d045389e686eebaa02daa5a90069e72
Leifdin <pavolpolonec@gmail.com>
2025-11-14 15:23:35 +0100

    styly

--- part7/bloglist-frontend/src/components/Navbar.jsx
+++ part7/bloglist-frontend/src/components/Navbar.jsx
@@ -1,12 +1,32 @@
 import React from "react";
-import { Link } from "react-router";
-
+import { Link, useNavigate } from "react-router";
+import styled from "styled-components";
 const Navbar = () => {
+  const navigate = useNavigate();
+  const NavLink = styled(Link)`
+    font-size: 1.5em;
+    color: white;
+    text-decoration: none;
+    cursor: pointer;
+    text-transform: uppercase;
+    padding: 10px;
+    &:hover,
+    &:focus {
+      background: #838383ff;
+      color: #c4c4c4ff;
+    }
+  `;
+  const NavDiv = styled.div`
+    display: flex;
+    flex-flow: row;
+    padding: 5px;
+    background: #505050;
+  `;
   return (
-    <div style={{ display: "flex", flexFlow: "row", gap: "15px" }}>
-      <Link to="/">blogs</Link>
-      <Link to="/users">users</Link>
-    </div>
+    <NavDiv>
+      <NavLink to="">blogs</NavLink>
+      <NavLink to="/users">users</NavLink>
+    </NavDiv>
   );
 };
 
